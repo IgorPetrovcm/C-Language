0054dcd3998dea6c7da402cb992ce5744e6fc648
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-04-05 23:59:56 +0300

    -

---
+++ College/Семестр2/pr9/calculator.c
@@ -0,0 +1,50 @@
+# include <stdio.h>
+
+int plus(int a, int b);
+
+int minus(int a, int b);
+
+int division(int a, int b);
+
+int multiplication(int a, int b);
+
+int main () 
+{
+    int input;
+
+    int (*operations[4]) (int, int) = {plus, minus, division, multiplication};
+
+    do {
+        int firstNumber;
+        int secondNumber;
+
+        printf("\n1. +\n2. -\n3. /\n4. *\n");
+        scanf_s("%d", &input);
+
+        printf("\nPlease, input first number: ");
+        scanf_s("%d", &firstNumber);
+
+        printf("\nPlease, input second number: ");
+        scanf_s("%d", &secondNumber);
+
+        printf("\n%d", operations[input - 1](firstNumber, secondNumber));
+        
+    } while (input != 46);
+    
+}
+
+int plus (int a, int b){
+    return a + b;
+}
+
+int minus (int a, int b){
+    return a - b;
+}
+
+int division (int a, int b){
+    return a / b;
+}
+
+int multiplication (int a, int b){
+    return a * b;
+}
